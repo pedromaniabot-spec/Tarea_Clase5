8e44cd82d92be3b6aae79979f15e66ac164b0c5f
pedromaniabot-spec <pedromaniabot@gmail.com>
2025-11-19 18:48:50 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
+emailjs.init("AQUI VA EL CÓDGIO DE LA API"); 
 
+document.getElementById('personalDataForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const firstName = document.getElementById('firstName').value;
+  const lastName = document.getElementById('lastName').value;
+  const email = document.getElementById('email').value;
+  const phone = document.getElementById('phone').value;
+  const message = document.getElementById('message').value;
+
+  emailjs.send("CODIGO DEL SERVICIO", "CODIGO DE LA PLANTILLA", {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    phone: phone,
+    message: message
+  })
+  
+  .then(() => {
+    document.getElementById('status-message').innerText = "Datos enviados con éxito.";
+    document.getElementById('personalDataForm').reset();
+  })
+  .catch((error) => {
+    console.error("Error al enviar los datos:", error);
+    document.getElementById('status-message').innerText = "Hubo un error al enviar los datos.";
+  });
+});
